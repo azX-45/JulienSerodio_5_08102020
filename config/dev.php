f4f903ecf111f03e918d3a47bf61f5b4d156ab95
azX-45 <wazxwooz@gmail.com>
2020-10-08 10:40:30 +0200

    prod php

--- config/dev.php
+++ config/dev.php
@@ -1,7 +1,7 @@
 <?php
 
 const HOST = 'localhost';
-const DB_NAME = 'projet5';
+const DB_NAME = 'blog';
 const CHARSET = 'utf8';
 const DB_HOST = 'mysql:host='.HOST.';dbname='.DB_NAME.';charset='.CHARSET;
 const DB_USER = 'root';
---
+++ config/prod.php
@@ -0,0 +1,8 @@
+<?php
+
+const HOST = 'serodiojrm554.mysql.db';
+const DB_NAME = 'serodiojrm554';
+const CHARSET = 'utf8';
+const DB_HOST = 'mysql:host='.HOST.';dbname='.DB_NAME.';charset='.CHARSET;
+const DB_USER = 'serodiojrm554';
+const DB_PASS = ''; 
